68d72d5a8881c320591d19fd57026fc3fcabebb3
miia <miia@com.tw>
2020-01-07 14:29:17 +0800

    1st

--- app/src/main/java/com/fju/member/NicknameActivity.java
+++ app/src/main/java/com/fju/member/NicknameActivity.java
@@ -2,6 +2,7 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class NicknameActivity extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class NicknameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nickname);
+
     }
 }
---
+++ app/src/main/java/com/fju/member/SharedPreferences.java
@@ -0,0 +1,5 @@
+package com.fju.member;
+
+public class SharedPreferences {
+
+}
